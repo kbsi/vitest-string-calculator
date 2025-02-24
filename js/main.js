df7778c8a4d73dc90c0d8c9e89fe7ebe33a6e06f
Kevin Boulais <>
2025-02-24 15:54:24 +0100

    Refactor add function to filter out numbers greater than 1000 and update tests for improved clarity and structure

--- js/main.js
+++ js/main.js
@@ -13,17 +13,18 @@ export function add(numbersStr) {
     var numbers;
     if (delimiter.startsWith('[') && delimiter.endsWith(']')) {
         delimiter = delimiter.slice(1, -1).split('][').map(d => d.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|');
-        numbers = numbersStr.split(new RegExp(`(${delimiter})|\n`)).map(Number);
     } else {
         delimiter = delimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-        numbers = numbersStr.split(new RegExp(`[${delimiter}\n]`)).map(Number);
     }
+    
+    numbers = numbersStr.split(new RegExp(`(${delimiter})|\n`))
+                        .map(Number)
+                        .filter(n => n <= 1000);
     
     var negatives = numbers.filter(n => n < 0);
     if (negatives.length) {
         throw new Error(`Negatives not allowed. ${negatives.join(' ')}`);
     }
-    numbers = numbers.filter(n => n <= 1000);
 
     return numbers.reduce((a, b) => a + b);
 
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,54 +1,61 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { add } from '../js/main.js'
 
-it('add(numbersStr) return 0 with empty', () => {
-  expect(add('')).toBe(0)
+describe('test of the function add(numbersStr)', () =>{
+    it('return 0 with empty', () => {
+        expect(add('')).toBe(0)
+      })
+      
+      it('return 1 with "1"', () => {
+          expect(add('1')).toBe(1)
+      })
+      
+      it('return 3 with "1,2"', () => {
+          expect(add('1,2')).toBe(3)
+      })
+
+      it('return 3 with "a,2,re,1"', () => {
+        expect(add('a,2,re,1')).toBe(3)
+        })
+      
+      it('return 6 with "1\n2,3"', () => {
+          expect(add('1\n2,3')).toBe(6)
+      })
+      
+      it('with "1,\n" is not valid', () => {
+          expect(() => add('1,\n')).toThrow('Invalid input')
+      })
+      
+      it('return 3 with "//;\n1;2"', () => {
+          expect(add('//;\n1;2')).toBe(3)
+      })
+      
+      it('with "-1,2" is not valid', () => {
+          expect(() => add('-1')).toThrow('Negatives not allowed. -1')
+      })
+      
+      it('with "-1,-2" is not valid', () => {
+          expect(() => add('-1,-2')).toThrow('Negatives not allowed. -1 -2')
+      })
+      
+      //Les nombres strictement plus grand que 1000 doivent être ignorés
+      it('return 3 with "1\n2,1002"', () => {
+          expect(add('1\n2,1002')).toBe(3)
+      })
+      
+      //Les séparateurs peuvent être définis par plusieurs caractères
+      it('return 6 with "//[***]\n1***2***3"', () => {
+          expect(add('//[***]\n1***2***3')).toBe(6)
+      })
+      
+      //Il est possible de définir plusieurs séparateurs 
+      it('return 6 with "//[*][%]\n1*2%3"', () => {
+          expect(add('//[*][%]\n1*2%3')).toBe(6)
+      })
+      
+      //Enfin, vérifier que le cas suivant fonctionne.
+      it('return 6 with "//[**][%%]\n1**2%%3"', () => {
+          expect(add('//[**][%%]\n1**2%%3')).toBe(6)
+      })
 })
 
-it('add(numbersStr) return 1 with "1"', () => {
-    expect(add('1')).toBe(1)
-})
-
-it('add(numbersStr) return 3 with "1,2"', () => {
-    expect(add('1,2')).toBe(3)
-})
-
-it('add(numbersStr) return 6 with "1\n2,3"', () => {
-    expect(add('1\n2,3')).toBe(6)
-})
-
-it('add(numbersStr) with "1,\n" is not valid', () => {
-    expect(() => add('1,\n')).toThrow('Invalid input')
-})
-
-it('add(numbersStr) return 3 with "//;\n1;2"', () => {
-    expect(add('//;\n1;2')).toBe(3)
-})
-
-it('add(numbersStr) with "-1,2" is not valid', () => {
-    expect(() => add('-1,2')).toThrow('Negatives not allowed. -1')
-})
-
-it('add(numbersStr) with "-1,-2" is not valid', () => {
-    expect(() => add('-1,-2')).toThrow('Negatives not allowed. -1 -2')
-})
-
-//Les nombres strictement plus grand que 1000 doivent être ignorés
-it('add(numbersStr) return 3 with "1\n2,1002"', () => {
-    expect(add('1\n2,1002')).toBe(3)
-})
-
-//Les séparateurs peuvent être définis par plusieurs caractères
-it('add(numbersStr) return 6 with "//[***]\n1***2***3"', () => {
-    expect(add('//[***]\n1***2***3')).toBe(6)
-})
-
-//Il est possible de définir plusieurs séparateurs 
-it('add(numbersStr) return 6 with "//[*][%]\n1*2%3"', () => {
-    expect(add('//[*][%]\n1*2%3')).toBe(6)
-})
-
-//Enfin, vérifier que le cas suivant fonctionne.
-it('add(numbersStr) return 6 with "//[**][%%]\n1**2%%3"', () => {
-    expect(add('//[**][%%]\n1**2%%3')).toBe(6)
-})
